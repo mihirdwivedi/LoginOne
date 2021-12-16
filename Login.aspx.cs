b79ebc45e77fe722298825de6dcd85d424bf76a9
mihirdwivedi <40501792+mihirdwivedi@users.noreply.github.com>
2021-12-16 12:09:35 +0530

    Add files via upload

---
+++ Login.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Login_One
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string strcon = ConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString;
+            
+            SqlConnection con = new SqlConnection(strcon);
+
+           
+            SqlCommand cmd = new SqlCommand("select * from Login where Email=@username and Password=@Password", con);
+            cmd.Parameters.AddWithValue("@username", TextBox1.Text);
+            cmd.Parameters.AddWithValue("@Password", TextBox2.Text);
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            con.Open();
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+
+            if (dt.Rows.Count > 0)
+            {
+                Response.Redirect("Redirect.aspx");
+            }
+            else
+            {
+                Label1.Text = "Your username and Password is incorrect";
+                Label1.ForeColor = System.Drawing.Color.Red;
+
+            }
+        }
+    }
+}
